06491b0246b0c7a740d7a49dc1e0ebffe6ab8e32
OmSoma1912 <61110267+OmSoma1912@users.noreply.github.com>
2021-02-05 17:51:18 +0530

    Add files via upload

---
+++ Python Projects/C97.py
@@ -0,0 +1,13 @@
+myName = "Om"
+myAge = 13
+
+age  = int(input("Enter your age : "))
+if (age > 18):
+    print ("You are an adult. You can have freedom")
+
+elif (age > 12):
+    print ("You are a teenager, therefore you don't have freedom")
+
+else:
+    print ("You are a kid, you don't need freedom")
+
---
+++ Python Projects/C97_2.py
@@ -0,0 +1,5 @@
+myFriendList = ["Ram", "Radha", "Nivedita"]
+count = 1000000
+while (count >= 0):
+    print(count)
+    count = count - 1
